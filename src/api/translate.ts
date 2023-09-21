10440a07640b5927ba94c7d671ff8a83695ffb4a
Kuon-dev <aaronlyn88@gmail.com>
2023-09-21 11:52:57 +0800

    refactor: changed to cheerio from jsdom

--- src/api/translate.ts
+++ src/api/translate.ts
@@ -1,48 +1,64 @@
 import OpenAI from "openai";
-import { JSDOM } from 'jsdom';
+import cheerio from "cheerio";
 
 export async function translate(lang: string, htmlString: string, htmlTags: string[]): Promise<string> {
-    const openai = new OpenAI({
-        apiKey: (import.meta as any).env.VITE_API_KEY ?? '',
-        // organization: "org-35EJHLrwW0VUMmMRARvsrpCW"
-    });
-    const dom = new JSDOM(htmlString);
-    const { window } = dom;
-    const { document } = window;
+  const openai = new OpenAI({
+    apiKey: (import.meta as any).env.VITE_API_KEY ?? "",
+  });
 
-    for (const tag of Array.from(document.querySelectorAll(htmlTags.join(','))) as Element[]) {
-        const newChildren: ChildNode[] = [];
-        
-        for (const child of Array.from(tag.childNodes)) {
-            if (child.nodeType === 3) {  // Node.TEXT_NODE
-                const textContent = child.textContent || "";
-                if (textContent.trim() === "") continue;
+  const $ = cheerio.load(htmlString);
 
-                const response = await openai.chat.completions.create({
-                    model: "gpt-3.5-turbo",
-                    messages: [
-                        {role: "system", content: `You will be provided with a sentence in English, and your task is to translate it into ${lang}`},
-                        {role: "user", content: `Translate '${textContent}' into ${lang}. Provide only the translated text. No elaboration.`}
-                    ]
-                });
+  for (const tag of htmlTags) {
+    $(tag).each((index, element) => {
+      const htmlContent = $(element).html();
+      if (!htmlContent) return;
 
-                const translatedText = response.choices[0].message.content ?? '';
-                const newChild = document.createTextNode(translatedText);
-                newChildren.push(newChild);
-            } else {
-                newChildren.push(child);
+      const textNodes: { index: number; text: string }[] = [];
+
+      $(element)
+        .contents()
+        .each((i, el) => {
+          if (el.type === "text") {
+            const text = $(el).text().trim();
+            if (text) {
+              textNodes.push({ index: i, text });
             }
-        }
+          }
+        });
 
-        while (tag.firstChild) {
-            tag.removeChild(tag.firstChild);
-        }
+      /* eslint-disable-next-line */
+      const translateTasks = textNodes.map(({ index, text }) =>
+        openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content: `You will be provided with a sentence in English, and your task is to translate it into ${lang}`,
+            },
+            {
+              role: "user",
+              content: `Translate '${text}' into ${lang}. Provide only the translated text. No elaboration.`,
+            },
+          ],
+        })
+      );
 
-        for (const newChild of newChildren) {
-            tag.appendChild(newChild);
-        }
-    }
+      Promise.all(translateTasks).then((responses) => {
+        responses.forEach((response, i) => {
+          const translatedText = response.choices[0].message.content || "";
+          const textNode = $(element).contents().get(textNodes[i].index);
+          if (textNode) {
+            $(textNode).replaceWith(translatedText);
+          }
+        });
+      });
+    });
+  }
 
-    return (dom.serialize());
+  // Wait for translations to finish
+  // This is just for demonstration. In a real-world application,
+  // you would need a more robust way to confirm that all translations are complete.
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  return $.html();
 }
-
