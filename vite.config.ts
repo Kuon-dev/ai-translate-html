d33986406fe2173eec1649e9dce9255ed6f3fb75
Kuon-dev <aaronlyn88@gmail.com>
2023-10-03 12:01:35 +0800

    feat: modified wasm, set target compiler to esnext

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,26 @@ import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { qwikReact } from "@builder.io/qwik-react/vite";
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+// import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact(), wasm(), topLevelAwait()],
+    build: {
+      target: 'esnext'
+    },
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    },
+  },
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      qwikReact(),
+      wasm(),
+      // topLevelAwait(),
+    ],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
