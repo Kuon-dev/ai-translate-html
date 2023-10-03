3ab812ec27041fc7b811fde5c2308b6700fb58be
Kuon-dev <aaronlyn88@gmail.com>
2023-10-03 11:05:55 +0800

    feat: added wasm and top-level-await plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { qwikReact } from "@builder.io/qwik-react/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact(), wasm(), topLevelAwait()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
